54eb91e37436037570d1d151ade912a6a8617b98
Andrey Makhnev <ray.makhnev@gmail.com>
2020-09-20 00:27:17 +0300

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from 'routes';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h3>Welcome to react hooks!</h3>
+      <Router>
+        <Routes/>
+      </Router>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
